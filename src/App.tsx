514707384aac30b7e92eb363616402aed4db1da3
taodtu <taodtu.gmail.com>
2019-10-02 18:07:28 +0100

    create Header and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./component/layout/Header";
+import "./App.css";
+import { UserContext } from "./component/store/UserContext";
 
 const App: React.FC = () => {
+  const [user, setUser] = useState("jessjelly");
+
   return (
     <div className="App">
-      
+      <UserContext.Provider value={user}>
+        <Header />
+      </UserContext.Provider>
     </div>
   );
-}
+};
 
 export default App;
