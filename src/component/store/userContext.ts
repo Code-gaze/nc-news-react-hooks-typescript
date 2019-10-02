027fed5d5fb134ccb36b43903db0a0a5abf6bf79
taodtu <taodtu.gmail.com>
2019-10-02 17:32:29 +0100

    add string type for userContext

--- src/component/store/userContext.ts
+++ src/component/store/userContext.ts
@@ -1,3 +1,3 @@
-import React, { createContext } from "react";
+import { createContext } from "react";
 
-export const userContext = createContext("");
+export const userContext = createContext <string>("");
