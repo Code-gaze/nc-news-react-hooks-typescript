0f64e597379dd14c63f8b9ac623a0ecf5cd336b0
taodtu <taodtu.gmail.com>
2019-10-05 18:09:13 +0100

    change Vote component to tsx

--- src/component/button/Vote.tsx
+++ src/component/button/Vote.tsx
@@ -1,12 +1,52 @@
-import * as React from 'react';
-import { Button } from '@material-ui/core';
+import * as React from "react";
+import { Button } from "@material-ui/core";
+import { IArticle } from "../types";
 
 interface IVoteProps {
+  votes: number;
+  id: number;
+  handleVote: (id: number, body: { inc_votes: number }) => Promise<IArticle>;
 }
 
-const Vote: React.FunctionComponent<IVoteProps> = (props) => {
-    const [vote, setVote]= React.useState(0)
-  return ;
+const Vote: React.FunctionComponent<IVoteProps> = ({
+  votes,
+  id,
+  handleVote
+}) => {
+  const [vote, setVote] = React.useState(0);
+  const updateVote = (change: number) => {
+    setVote(vote + change);
+    handleVote(id, { inc_votes: change }).catch(error =>
+      setVote(vote - change)
+    );
+  };
+  return (
+    <div className="vote-block">
+      <div className="vote">
+        <Button
+          variant="outlined"
+          size="small"
+          color="primary"
+          disabled={vote > 0}
+          onClick={() => updateVote(1)}
+        >
+          + vote!
+        </Button>
+      </div>
+      <p>Votes: {votes + vote}</p>
+      <div className="vote">
+        <Button
+          variant="outlined"
+          size="small"
+          color="secondary"
+          disabled={vote < 0}
+          onClick={() => updateVote(-1)}
+        >
+          - vote!
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Vote;
